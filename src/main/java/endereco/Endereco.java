4f651c8d237025110513a85b7131c7a692595c43
Omega050 <mauro-jr50@hotmail.com>
2024-04-19 00:06:55 -0300

    Done

--- src/main/java/endereco/Endereco.java
+++ src/main/java/endereco/Endereco.java
@@ -1,18 +1,66 @@
 package endereco;
 
 public class Endereco {
-    public int numero;
-    public String Complemento;
-    public String CEP;
-    public Bairro bairro;
-    public Logradouro logradouro;
-    public TipoEndereco tipoEndereco;
-    
+    private int numero;
+    private String Complemento;
+    private String CEP;
+    private Bairro bairro;
+    private Logradouro logradouro;
+    private TipoEndereco tipoEndereco;
+
     public Endereco(int numero, String Complemento, String CEP, Bairro bairro, Logradouro logradouro, TipoEndereco tipoEndereco) {
         this.numero = numero;
         this.Complemento = Complemento;
         this.CEP = CEP;
         this.bairro = bairro;
         this. logradouro = logradouro;
+    }
+
+    public int getNumero() {
+        return this.numero;
+    }
+
+    public void setNumero(int numero) {
+        this.numero = numero;
+    }
+
+    public String getComplemento() {
+        return this.Complemento;
+    }
+
+    public void setComplemento(String Complemento) {
+        this.Complemento = Complemento;
+    }
+
+    public String getCEP() {
+        return this.CEP;
+    }
+
+    public void setCEP(String CEP) {
+        this.CEP = CEP;
+    }
+
+    public Bairro getBairro() {
+        return this.bairro;
+    }
+
+    public void setBairro(Bairro bairro) {
+        this.bairro = bairro;
+    }
+
+    public Logradouro getLogradouro() {
+        return this.logradouro;
+    }
+
+    public void setLogradouro(Logradouro logradouro) {
+        this.logradouro = logradouro;
+    }
+
+    public TipoEndereco getTipoEndereco() {
+        return this.tipoEndereco;
+    }
+
+    public void setTipoEndereco(TipoEndereco tipoEndereco) {
+        this.tipoEndereco = tipoEndereco;
     }
 }
