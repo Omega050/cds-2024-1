609d02f7ea8aecf2f2dd10ced41ac5446a9f8ecc
Omega050 <mauro-jr50@hotmail.com>
2024-04-19 00:01:55 -0300

    Pessoa Endereco

--- src/main/java/endereco/PessoaFisica.java
+++ src/main/java/endereco/PessoaFisica.java
@@ -6,6 +6,7 @@ public class PessoaFisica {
     private String nome;
     private String sexo;
     private Date dataNascimento;
+    private Endereco endereco;
 
     public PessoaFisica(String nome, String sexo, Date dataNascimento) {
         this.nome = nome;
@@ -35,5 +36,13 @@ public class PessoaFisica {
 
     public void setDataNascimento(Date dataNascimento) {
         this.dataNascimento = dataNascimento;
+    }
+
+    public Endereco getEndereco() {
+        return endereco;
+    }
+    
+    public void setEndereco(Endereco endereco) {
+        this.endereco = endereco;
     }
 }
