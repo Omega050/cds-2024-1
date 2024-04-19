a91b98dbc6b192ec3ef90fd6f79246a32d6c1228
Omega050 <mauro-jr50@hotmail.com>
2024-04-18 23:54:07 -0300

    Logradouro Final

--- src/main/java/endereco/Logradouro.java
+++ src/main/java/endereco/Logradouro.java
@@ -2,4 +2,10 @@ package endereco;
 
 public class Logradouro {
     public String nome;
+    public TipoLogradouro tipoLogradouro;
+
+    public Logradouro(String nome, TipoLogradouro tipoLogradouro) {
+        this.nome = nome;
+        this.tipoLogradouro = tipoLogradouro;
+    } 
 }
